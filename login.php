ae126ef6fff9c468d79dcb65413eb9226e3e7b47
Farhana Haque Sumi <farhana.haque.cse@ulab.edu.bd>
2021-01-28 17:20:29 +0900

    Update login.php

--- login.php
+++ login.php
@@ -6,6 +6,9 @@ $password=$_POST["pass"];
 
 include ('connect.php');
 
+
+try{
+    
 $sql = "SELECT UserID, Email FROM Registered_User WHERE UserID='$uid' AND Password='$password'";
     $results = mysqli_query($conn, $sql);
 
@@ -17,12 +20,16 @@ $sql = "SELECT UserID, Email FROM Registered_User WHERE UserID='$uid' AND Passwo
 else {
 
    
-
-    header('location: https://kobelcosign.000webhostapp.com/loginpage.php');
+    echo "<script>
+    alert('wrong User ID and Password');window.location = 'https://kobelcosign.000webhostapp.com/loginpage.php'
+    </script>";
+    //header('location: https://kobelcosign.000webhostapp.com/loginpage.php');
  
 }
-  
-
+}
+catch (Exception $e){
+    echo $e->getMessage();
+}
 $conn->close();
 
 ?>
