4634facedb1d3e858304773c4bffa3b22c19b620
Farhana Haque Sumi <farhana.haque.cse@ulab.edu.bd>
2021-01-28 17:23:38 +0900

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,6 @@
 <?php
+include ('connect.php');
+if(isset($_POST['submit'])){
 $name=$_POST["name"];
 $email=$_POST["email"];
 $uid=$_POST["uid"];
@@ -7,19 +9,33 @@ $token=bin2hex(random_bytes(15));
 $mobile=$_POST["mobile"];
 $company=$_POST["company"];
 
-include ('connect.php');
+
 $sql = "Insert into Registered_User (Name, Email, UserID,Password, Token ,Mobile_Number, Company) values ('$name','$email','$uid','$password','$token','$mobile','$company')";
-if (mysqli_query($conn, $sql)) {
-echo "Records inserted successfully.";
 
+$sql1= mysqli_query($conn,"SELECT Email from Registered_User where Email='$email' ");
 
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+if(mysqli_num_rows($sql1)>0){
+    echo "<script>
+    alert('email already exists');window.location = 'https://kobelcosign.000webhostapp.com/loginpage.php'
+    </script>";
+    
 }
-
+else{
+    
+    if(mysqli_query($conn, $sql)){
+        echo "Records inserted successfully.";
+    }
+   
+   else{
+echo "<script>
+    alert('User ID already exists');window.location = 'https://kobelcosign.000webhostapp.com/loginpage.php'
+    </script>";
+       
+   }
+ 
+ }
+}
 $conn->close();
 
 ?>
-
-
 
