1c15732c9fea9f41bbbfce0e6d78a516032ff9d2
Farhana Haque Sumi <farhana.haque.cse@ulab.edu.bd>
2021-01-28 17:16:56 +0900

    Update forget.php

--- forget.php
+++ forget.php
@@ -3,37 +3,38 @@
 if(isset ($_POST['submit'])){
 $email = $_POST['email'];
 include ('connect.php');
-$emailquery = "select UserID, Email from Registered_User where Email='$email'";
 
-$result = $conn->query($emailquery);
-$query = mysqli_query($conn,$emailquery);
-$emailcount= mysqli_num_rows($query);
 
-if($emailcount)
-{
+$sql = "select UserID, Token from Registered_User where Email='$email'";
+$result = mysqli_query($conn, $sql);
 
-$userdata = mysqli_fetch_assoc($query,MYSQLI_ASSOC);
-$uid = $userdata['UserID'];
-$token = $userdata['Token'];
-$subject = "Password Reset";
-$body= "Hi, $uid. click here https://kobelcosign.000webhostapp.com/resetpass.php?token=$token";
-   $sender_email="From: farhanasumi893@gmail.com";
-   
- if (mail($email,$subject,$body,$sender_email) )
- {
- //$_SESSION['msg']="check email $email";
- header('location: loginpage.php');
- }
- else{
- echo "email sending failed";
- }
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+  $row = mysqli_fetch_assoc($result);
+  $uid = $row["UserID"];
+  $token = $row["Token"];
+  $subject = "Password Reset";
+  $body= "Hi, $uid. click here https://kobelcosign.000webhostapp.com/resetpass.php?Token=$token";
+  $sender_email="From: farhanasumi893@gmail.com";
+     if (mail($email,$subject,$body,$sender_email) )
+          {
+             //$_SESSION['msg']="check email $email";
+             header('location: loginpage.php');
+          }
+          
+    else
+        {
+            echo "email sending failed";
+        }      
+          
+          
+        }
 }
-}
- else{
- echo "no email";
- }
+        
 
-$conn->close();
+   else{
+        echo "no email";
+  }
 
+    $conn->close();
 ?>
-
